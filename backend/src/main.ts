023edb71655f137fa59dedda69b147afe9d68ba1
sardar-ali <alisardar465@gmail.com>
2025-04-13 19:09:14 +0400

    change port fo frontend and backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
